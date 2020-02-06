36188403d4035a385d250615f31ec159dd348a72
iambillmccann <bill.mccann@gmail.com>
2020-02-06 15:22:33 -0500

    Completed the first problem ... Multiples of 3 or 5

--- ProjectEuler/src/eulerlibrary/Problems/Multiple3or5.java
+++ ProjectEuler/src/eulerlibrary/Problems/Multiple3or5.java
@@ -1,8 +1,37 @@
 package eulerlibrary.Problems;
 
-import java.util.concurrent.TimeUnit;
+import eulerlibrary.MathLibrary.MathLibrary;
 
 public class Multiple3or5 implements IEulerSolution {
+
+    final long RANGE_START = 1;
+    final long RANGE_STOP  = 1000;
+
+    /**
+     *  Determine the number of terms in an Arithmetic Progression
+     * 
+     * @param rangeStart First number in the range
+     * @param rangeStop  Last number in the range
+     * @param difference Difference between terms
+     * @return The number of terms
+     */
+    private long NumberOfTerms (long rangeStart, long rangeStop, long difference)
+    {
+        return (rangeStop - rangeStart) / difference;
+    }
+
+    /**
+     * Determine the last number in an Arithmetic Progression
+     * @param numberOfTerms The number of terms
+     * @param firstTerm     The first number
+     * @param difference    The difference between terms
+     * @return              The largest value of the sequence
+     */
+    private long LastTerm (long numberOfTerms, long firstTerm, long difference)
+    {
+        return firstTerm + difference * (numberOfTerms - 1);
+    }
+
     /**
      * If we list all the natural numbers below 10 that are multiples of 3 or 5, we
      * get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the
@@ -12,14 +41,19 @@ public class Multiple3or5 implements IEulerSolution {
      */
 
     @Override
-    public String Compute() {
-        try {
-            TimeUnit.MILLISECONDS.sleep(2333);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return "3.1415926";
+    public String Compute() 
+    {
+        Long sum3 = MathLibrary.ArithmeticSeries(NumberOfTerms(RANGE_START, RANGE_STOP, 3), 
+                                                 3, 
+                                                 LastTerm(NumberOfTerms(RANGE_START, RANGE_STOP, 3), 3, 3));
+        Long sum5 = MathLibrary.ArithmeticSeries(NumberOfTerms(RANGE_START, RANGE_STOP, 5), 
+                                                 5, 
+                                                 LastTerm(NumberOfTerms(RANGE_START, RANGE_STOP, 5), 5, 5));
+        Long sum15 = MathLibrary.ArithmeticSeries(NumberOfTerms(RANGE_START, RANGE_STOP, 15), 
+                                                 15, 
+                                                 LastTerm(NumberOfTerms(RANGE_START, RANGE_STOP, 15), 15, 15));
+
+        return Long.toString(sum3 + sum5 - sum15);
     }
 
 }
