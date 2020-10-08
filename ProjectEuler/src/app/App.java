bd26a3e37bb946e7cbf2bfa21be10f196d1dfad8
iambillmccann <bill.mccann@gmail.com>
2020-10-08 18:56:27 -0400

    Begin work on problem 13

--- ProjectEuler/src/app/App.java
+++ ProjectEuler/src/app/App.java
@@ -9,7 +9,7 @@ import eulerlibrary.MathLibrary.Utilities;
 public class App {
 
     private static String QUITSTRING = "Q";
-    private static int LASTPROBLEM = 12;
+    private static int LASTPROBLEM = 13;
 
     /**
      * getUserInput is a simple method for reading from the console.
